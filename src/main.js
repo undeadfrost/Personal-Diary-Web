b4db5840c29f8c771217faef4831f68b53db9565
Frost <undeadfrost@live.com>
2018-04-18 19:59:39 +0800

    完善目录结构，引入组件

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/views/users/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {msg: '这个是Home模板页'}
+        }
+    }
+</script>
+
+<style scoped>
+    body {
+        background-color: #ff0000;
+    }
+</style>
